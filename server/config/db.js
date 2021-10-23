7ad1eafeda677c0e5d4b7d91103998f37b9d9b1d
Jay Bhadreshwara <bhadreshwara99@gmail.com>
2021-10-23 13:29:48 -0400

    commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //"URI" : "mongodb://localhost/contact_list"
+  URI: "mongodb+srv://jay:jay@cluster0.boif0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  Secret: "JayPortfolioSecretKey",
+};
---
+++ server/models/business.js
@@ -0,0 +1,16 @@
+let mongoose = require("mongoose");
+
+//create a model class
+let businessModel = mongoose.Schema(
+  {
+    name: String,
+    number: Number,
+    email: String,
+  },
+  {
+    collection: "contact",
+  }
+);
+
+module.exports = mongoose.model("contact", businessModel);
+// 'business'
